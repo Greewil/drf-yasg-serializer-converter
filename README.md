9a64fcbfa681424552f4b52156d3a2d31a7467e9
Shishkin Sergey <shishkinsergeydmitrievich@gmail.com>
2024-06-27 02:47:04 +0300

    feat: basic logic

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # drf-yasg-serializer-converter
+[![release](https://badgen.net/github/release/Greewil/drf-yasg-serializer-converter/stable)](https://github.com/Greewil/drf-yasg-serializer-converter/releases)
+[![Last updated](https://img.shields.io/github/release-date/Greewil/drf-yasg-serializer-converter?label=updated)](https://github.com/Greewil/drf-yasg-serializer-converter/releases)
+
 Converts django serializer to openapi schema.
---
+++ src/drf_yasg_serializer_converter/swaggers_schema_properties/openapi_schema_converters.py
@@ -0,0 +1,110 @@
+import inspect
+from typing import Dict, List, Tuple
+
+from django.db import models
+from drf_yasg import openapi
+from rest_framework.serializers import Serializer, BaseSerializer
+
+rest_framework_openapi_field_mapping = {
+    "ListField": openapi.TYPE_ARRAY,
+    "CharField": openapi.TYPE_STRING,
+    "BooleanField": openapi.TYPE_BOOLEAN,
+    "FloatField": openapi.TYPE_NUMBER,
+    "DateTimeField": openapi.TYPE_STRING,
+    "IntegerField": openapi.TYPE_INTEGER,
+    "SerializerMethodField": openapi.TYPE_STRING
+}
+
+additional_info_fields_conversions = {
+    "read_only": "readOnly",
+    "allow_null": "x-nullable",
+    "max_value": "maximum",
+    "min_value": "minimum",
+    "max_length": "maxLength",
+    "min_length": "minLength",
+}
+
+
+def _get_additional_properties(field, serializer_meta_model_field: models.Field) -> Dict:
+    additional_properties = {}
+    dict_field = field.__dict__
+    if serializer_meta_model_field is not None:
+        if serializer_meta_model_field.default != models.fields.NOT_PROVIDED:
+            additional_properties['default'] = serializer_meta_model_field.default
+        if serializer_meta_model_field.blank and serializer_meta_model_field.null:
+            additional_properties['x-nullable'] = serializer_meta_model_field.blank
+    for field_property, final_property in additional_info_fields_conversions.items():
+        if dict_field.get(field_property):
+            additional_properties[final_property] = dict_field[field_property]
+    return additional_properties
+
+
+def _get_required(field: str) -> bool:
+    if "required=False" not in field:
+        return True
+    else:
+        return False
+
+
+def _get_description(field: str) -> [str, None]:
+    if "help_text=" in field:
+        return field.split("help_text='")[-1].split("'")[0]
+
+
+def _parse_rest_framework_field(field, serializer_meta_model_field: models.Field) -> Tuple[bool, openapi.Schema]:
+    additional_properties = _get_additional_properties(field, serializer_meta_model_field)
+    field_str = str(field)
+    rest_framework_field_type = field_str.split("(")[0]
+    openapi_field_type = rest_framework_openapi_field_mapping.get(rest_framework_field_type, openapi.TYPE_STRING)
+    field_description = _get_description(field_str)
+    field_required = _get_required(field_str)
+    return field_required, openapi.Schema(type=openapi_field_type,
+                                          description=field_description,
+                                          **additional_properties)
+
+
+def _parse_serializer(serializer: Serializer) -> Tuple[Dict[str, openapi.Schema], List[str]]:
+    properties = {}
+    required_properties = []
+    # checking if there any chance to get NestedSerializer from non default Serializer
+    if (isinstance(serializer, BaseSerializer) and not isinstance(serializer, Serializer)) \
+            and hasattr(serializer, 'child') and isinstance(serializer.child, Serializer):
+        serializer = serializer.child
+    # trying to get Model from Serializer
+    serializer_class = type(serializer)
+    serializer_meta_model = None
+    if hasattr(serializer_class, 'Meta') and hasattr(serializer_class.Meta, 'model'):
+        serializer_meta_model = serializer_class.Meta.model
+    # parsing fields
+    for k, v in serializer.get_fields().items():
+        serializer_meta_model_field = None
+        if serializer_meta_model is not None:
+            serializer_meta_model_field = getattr(serializer_meta_model, k).field
+        if v.__module__ == "rest_framework.fields":
+            property_required, properties[k] = _parse_rest_framework_field(v, serializer_meta_model_field)
+            if property_required:
+                required_properties.append(k)
+        elif isinstance(v, BaseSerializer):
+            object_properties, object_required_properties = _parse_serializer(v)
+            additional_properties = _get_additional_properties(v, serializer_meta_model_field)
+            field_description = _get_description(str(v))
+            if _get_required(str(v)):
+                required_properties.append(k)
+            properties[k] = openapi.Schema(type=openapi.TYPE_OBJECT,
+                                           description=field_description,
+                                           properties=object_properties,
+                                           required=object_required_properties,
+                                           **additional_properties)
+        else:
+            pass
+    return properties, required_properties
+
+
+def get_schema(serializer: Serializer, description: str = '') -> openapi.Schema:
+    """ Converts object of type Serializer or inherited from Serializer class to an openapi.Schema object. """
+    if inspect.isclass(serializer) and issubclass(serializer, Serializer):
+        serializer = serializer()
+    properties, required_properties = _parse_serializer(serializer)
+    return_openapi_schema = openapi.Schema(type=openapi.TYPE_OBJECT, properties=properties,
+                                           description=description, required=required_properties)
+    return return_openapi_schema
